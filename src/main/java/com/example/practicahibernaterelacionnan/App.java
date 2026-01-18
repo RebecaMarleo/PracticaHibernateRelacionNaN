3b4141d4a5c55780a60f1ae459695ad27e47ca7c
RebecaMarleo <rebecamarleo@outlook.es>
2026-01-18 23:38:20 +0100

    Realizados pequeños ajustes para release

---
+++ src/main/java/com/example/practicahibernaterelacionnan/App.java
@@ -0,0 +1,7 @@
+package com.example.practicahibernaterelacionnan;
+
+public class App {
+    public static void main(String[] args) {
+        Launcher.main(args);
+    }
+}
--- src/main/java/com/example/practicahibernaterelacionnan/Util/R.java
+++ src/main/java/com/example/practicahibernaterelacionnan/Util/R.java
@@ -1,16 +1,18 @@
 package com.example.practicahibernaterelacionnan.Util;
 
-import java.io.File;
-import java.io.InputStream;
 import java.net.URL;
 
 public class R {
 
     public static URL getProperties(String name) {
-        return Thread.currentThread().getContextClassLoader().getResource("Configuracion" + File.separator + name);
+        // File.separator es la barra hacia atrás "\" pero los archivos .jar necesitan usar la barra hacia delante "/" y da error
+        return Thread.currentThread().getContextClassLoader().getResource("Configuracion/" + name);
+//        return Thread.currentThread().getContextClassLoader().getResource("Configuracion" + File.separator + name);
     }
 
     public static URL getUI(String name) {
-        return Thread.currentThread().getContextClassLoader().getResource("ui" + File.separator + name);
+        // File.separator es la barra hacia atrás "\" pero los archivos .jar necesitan usar la barra hacia delante "/" y da error
+        return Thread.currentThread().getContextClassLoader().getResource("ui/" + name);
+//        return Thread.currentThread().getContextClassLoader().getResource("ui" + File.separator + name);
     }
 }
